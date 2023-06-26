b58f2339a15ec77417709415c56d21bf698f0ec8
TripleKay <triplekay.k3@gmail.com>
2023-06-26 19:41:58 +0630

    add crud .php

---
+++ src/Crud.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TripleKay\Crud;
+
+class Crud
+{
+    // Build your next great package.
+    public static function hi()
+    {
+        echo "first laravel packages";
+        // uses ScaffoldedLaravel::hi()
+    }
+}
